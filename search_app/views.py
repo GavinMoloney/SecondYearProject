5050560f2779b45cd070307c1e207582309d479f
Gavin Moloney <X00166625@mytudublin.ie>
2021-03-24 22:44:06 +0000

    search fix with category result

--- search_app/views.py
+++ search_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from store.models import Product
+from store.models import Product, Category
 from django.db.models import Q
 
 def searchResult(request):
@@ -7,5 +7,7 @@ def searchResult(request):
     query = None
     if 'q' in request.GET:
         query = request.GET.get('q')
-        products = Product.objects.all().filter(Q(name__contains=query) | Q(description__contains=query))
-    return render(request, 'search.html', {'query':query, 'products':products})
+        products = Product.objects.all().filter(Q(name__contains=query) | Q(description__contains=query) | Q(category__name__contains=query))
+    return render(request, 'search.html', {'query':query, 'products':products}) 
+
+
