0b1b835e97a1c69e2daacd320acd3ff81c1d35e0
Gavin Moloney <X00166625@mytudublin.ie>
2021-03-26 10:21:14 +0000

    newsletter integrity error fix

--- newsletter/forms.py
+++ newsletter/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
 class SubscriberForm(forms.Form):
-    email = forms.EmailField(label='Your email',
+    email = forms.EmailField(label='Please enter your email here',
                              max_length=100,
                              widget=forms.EmailInput(attrs={'class': 'form-control'}))
--- newsletter/views.py
+++ newsletter/views.py
@@ -8,30 +8,38 @@ import random
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
 
-# Helper Functions
+# random num for confirmation number
 def random_digits():
     return "%0.12d" % random.randint(0, 999999999999)
 
-@csrf_exempt
+@csrf_exempt   # disables csrf protection for this view
 def new(request):
     if request.method == 'POST':
-        sub = Subscriber(email=request.POST['email'], conf_num=random_digits())
-        sub.save()
-        message = Mail(
-            from_email=settings.FROM_EMAIL,
-            to_emails=sub.email,
-            subject='Newsletter Confirmation',
-            html_content='Thank you for signing up for my email newsletter! \
-                Please complete the process by \
-                <a href="{}?email={}&conf_num={}"> clicking here to \
-                confirm your registration</a>.'.format(request.build_absolute_uri('/confirm/'),
+        mail = request.POST['email']
+        if "@" in mail:         # error warning for @ missing
+            sub = Subscriber(email=request.POST['email'], conf_num=random_digits())
+            if Subscriber.objects.filter(email = mail).exists():   # catching integrity error from duplicate email added
+                return render(request, 'newsletter.html', {'email': sub.email, 'action': 'registered previously', 'form': SubscriberForm()})
+            else:
+                sub.save()
+                message = Mail(
+                    from_email=settings.FROM_EMAIL,
+                    to_emails=sub.email,
+                    subject='Newsletter Confirmation',
+                    html_content='Thank you for signing up for our Astronomy Shop email newsletter! \
+                        Please complete the process by \
+                        <a href="{}?email={}&conf_num={}"> clicking here to \
+                        confirm your registration</a>.'.format(request.build_absolute_uri('/confirm/'),
                                                     sub.email,
                                                     sub.conf_num))
-        sg = SendGridAPIClient(settings.SENDGRID_API_KEY)
-        response = sg.send(message)
-        return render(request, 'newsletter.html', {'email': sub.email, 'action': 'added', 'form': SubscriberForm()})
+                sg = SendGridAPIClient(settings.SENDGRID_API_KEY)   # uses sendgrid system
+                response = sg.send(message)
+                return render(request, 'newsletter.html', {'email': sub.email, 'action': 'added', 'form': SubscriberForm()})
+        else:
+            return render(request, 'newsletter.html', {'email': mail, 'action': 'is not a valid address, please try again', 'form': SubscriberForm()})
     else:
-        return render(request, 'newsletter.html', {'form': SubscriberForm()})
+            return render(request, 'newsletter.html', {'form': SubscriberForm()})
+
 
 def confirm(request):
     sub = Subscriber.objects.get(email=request.GET['email'])
