6a50a6c0fe719fd0d877bbede2d3b1e664bdfdda
Andrzej Zero <x00166801@mytudublin.ie>
2021-04-12 05:38:57 +0100

    management - category

--- management/urls.py
+++ management/urls.py
@@ -1,15 +1,25 @@
 from django.urls import path
-from .views import ManagementView, UserListView, UserDetailView, UserCreateView, UserDeleteView, UserUpdateView
+from .views import UserManagementView, UserListView, UserDetailView, UserCreateView, UserDeleteView, UserUpdateView
+from .views import ShopManagementView, CategoryListView, CategoryCreateView, CategoryDeleteView, CategoryUpdateView, CategoryDetailView
+
+
 
 
 app_name='management'
 
 
 urlpatterns = [
-    path('', ManagementView.as_view(), name = 'management'),
-    path('users/', UserListView.as_view(), name = 'users'),
+    path('', UserManagementView.as_view(), name = 'management'),
+    path('user_management/', UserListView.as_view(), name = 'users'),
     path('user_detail/<int:pk>', UserDetailView.as_view(), name = 'user_details'),
     path('user_add/', UserCreateView.as_view(), name = 'user_add'),
     path('user_delete/<int:pk>', UserDeleteView.as_view(), name = 'user_delete'),
     path('user_update/<int:pk>', UserUpdateView.as_view(), name = 'user_update'),
+
+    path('shop_management/', ShopManagementView.as_view(), name = 'manage_shop'),
+    path('shop_management/categories/', CategoryListView.as_view(), name = 'cats'),
+    path('shop_management/categories/<slug:slug>/', CategoryDetailView.as_view(), name = 'cat_details'),
+    path('shop_management/categories/category_add', CategoryCreateView.as_view(), name = 'cat_add'),
+    path('shop_management/categories/category_delete/<slug:slug>/', CategoryDeleteView.as_view(), name = 'cat_delete'),
+    path('shop_management/categories/category_update/<slug:slug>/', CategoryUpdateView.as_view(), name = 'cat_update'),
 ]
--- management/views.py
+++ management/views.py
@@ -1,12 +1,13 @@
 from django.views.generic import TemplateView
 from accounts.models import CustomUser
+from store.models import Category, Product
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import UpdateView, DeleteView, CreateView
 from django.urls import reverse_lazy, reverse
 from django.shortcuts import render, redirect
 from .forms import UserForm
 
-class ManagementView(TemplateView):
+class UserManagementView(TemplateView):
     template_name = 'mgmt.html'
 
 
@@ -38,3 +39,38 @@ class UserCreateView(CreateView):
     fields = ('username', 'password', 'first_name', 'last_name', 'email', 'is_staff','can_upload_images','groups',)
     template_name = 'user_add.html'
     success_url = reverse_lazy('management:users')
+
+
+class ShopManagementView(TemplateView):
+    template_name = 'shop_management.html'
+
+
+
+class CategoryListView(ListView):
+    model = Category
+    context_object_name = 'category_list'
+    template_name = 'categories.html'
+
+class CategoryDetailView(DetailView):
+    model = Category
+    template_name = 'category_details.html'
+
+
+class CategoryUpdateView(UpdateView):
+    model = Category
+    fields = ('slug', 'name', 'description', 'image', 'image_secondary',)
+    template_name = 'category_edit.html'
+    success_url = reverse_lazy('management:cats')
+
+
+class CategoryDeleteView(DeleteView):
+    model = Category
+    template_name = 'category_delete.html'
+    success_url = reverse_lazy('management:cats')
+    
+
+class CategoryCreateView(CreateView):
+    model = Category
+    fields = ('slug', 'name', 'description', 'image', 'image_secondary',)
+    template_name = 'category_add.html'
+    success_url = reverse_lazy('management:cats')
