454bba0453dfb650f7a8b9688e558f58837e3cc8
Andrzej Zero <x00166801@mytudublin.ie>
2021-04-16 21:03:55 +0100

    management product, profile, voucher, subscriber, newsletter

--- management/urls.py
+++ management/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from .views import UserManagementView, UserListView, UserDetailView, UserCreateView, UserDeleteView, UserUpdateView
 from .views import ShopManagementView, CategoryListView, CategoryCreateView, CategoryDeleteView, CategoryUpdateView, CategoryDetailView
+from .views import ProductListView, ProductCreateView, ProductDeleteView, ProductDetailView, ProductUpdateView
+from .views import VoucherListView, VoucherCreateView, VoucherDeleteView, VoucherDetailView, VoucherUpdateView
+from .views import ProfileListView, ProfileCreateView, ProfileDeleteView, ProfileDetailView, ProfileUpdateView
+from .views import SubscriberListView, SubscriberCreateView, SubscriberDeleteView, SubscriberDetailView, SubscriberUpdateView
+from .views import NewsletterListView, NewsletterCreateView, NewsletterDeleteView, NewsletterDetailView, NewsletterUpdateView
 
 
 
@@ -11,10 +16,28 @@ app_name='management'
 urlpatterns = [
     path('', UserManagementView.as_view(), name = 'management'),
     path('user_management/', UserListView.as_view(), name = 'users'),
-    path('user_detail/<int:pk>', UserDetailView.as_view(), name = 'user_details'),
+    path('user_detail/<int:pk>/', UserDetailView.as_view(), name = 'user_details'),
     path('user_add/', UserCreateView.as_view(), name = 'user_add'),
-    path('user_delete/<int:pk>', UserDeleteView.as_view(), name = 'user_delete'),
-    path('user_update/<int:pk>', UserUpdateView.as_view(), name = 'user_update'),
+    path('user_delete/<int:pk>/', UserDeleteView.as_view(), name = 'user_delete'),
+    path('user_update/<int:pk>/', UserUpdateView.as_view(), name = 'user_update'),
+
+    path('profiles/', ProfileListView.as_view(), name = 'profiles'),
+    path('profile_detail/<int:pk>/', ProfileDetailView.as_view(), name = 'profile_details'),
+    path('profile_add/', ProfileCreateView.as_view(), name = 'profile_add'),
+    path('profile_delete/<int:pk>/', ProfileDeleteView.as_view(), name = 'profile_delete'),
+    path('profile_update/<int:pk>/', ProfileUpdateView.as_view(), name = 'profile_update'),
+
+    path('subscribers/', SubscriberListView.as_view(), name = 'subscribers'),
+    path('subscriber_detail/<int:pk>/', SubscriberDetailView.as_view(), name = 'subscriber_details'),
+    path('subscriber_add/', SubscriberCreateView.as_view(), name = 'subscriber_add'),
+    path('subscriber_delete/<int:pk>/', SubscriberDeleteView.as_view(), name = 'subscriber_delete'),
+    path('subscriber_update/<int:pk>/', SubscriberUpdateView.as_view(), name = 'subscriber_update'),
+
+    path('newsletters/', NewsletterListView.as_view(), name = 'newsletters'),
+    path('newsletter_detail/<int:pk>/', NewsletterDetailView.as_view(), name = 'newsletter_details'),
+    path('newsletter_add/', NewsletterCreateView.as_view(), name = 'newsletter_add'),
+    path('newsletter_delete/<int:pk>/', NewsletterDeleteView.as_view(), name = 'newsletter_delete'),
+    path('newsletter_update/<int:pk>/', NewsletterUpdateView.as_view(), name = 'newsletter_update'),
 
     path('shop_management/', ShopManagementView.as_view(), name = 'manage_shop'),
     path('shop_management/categories/', CategoryListView.as_view(), name = 'cats'),
@@ -22,4 +45,16 @@ urlpatterns = [
     path('shop_management/categories/category_add', CategoryCreateView.as_view(), name = 'cat_add'),
     path('shop_management/categories/category_delete/<slug:slug>/', CategoryDeleteView.as_view(), name = 'cat_delete'),
     path('shop_management/categories/category_update/<slug:slug>/', CategoryUpdateView.as_view(), name = 'cat_update'),
+
+    path('shop_management/products/', ProductListView.as_view(), name = 'products'),
+    path('shop_management/products/<slug:slug>/', ProductDetailView.as_view(), name = 'prod_details'),
+    path('shop_management/products/product_add', ProductCreateView.as_view(), name = 'prod_add'),
+    path('shop_management/products/product_delete/<slug:slug>/', ProductDeleteView.as_view(), name = 'prod_delete'),
+    path('shop_management/products/product_update/<slug:slug>/', ProductUpdateView.as_view(), name = 'prod_update'),
+
+    path('shop_management/vouchers/', VoucherListView.as_view(), name = 'vouchers'),
+    path('shop_management/vouchers/<int:pk>/', VoucherDetailView.as_view(), name = 'voucher_details'),
+    path('shop_management/vouchers/product_add', VoucherCreateView.as_view(), name = 'voucher_add'),
+    path('shop_management/vouchers/product_delete/<int:pk>/', VoucherDeleteView.as_view(), name = 'voucher_delete'),
+    path('shop_management/vouchers/product_update/<int:pk>/', VoucherUpdateView.as_view(), name = 'voucher_update'),
 ]
--- management/views.py
+++ management/views.py
@@ -1,11 +1,16 @@
 from django.views.generic import TemplateView
-from accounts.models import CustomUser
-from store.models import Category, Product
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import UpdateView, DeleteView, CreateView
 from django.urls import reverse_lazy, reverse
 from django.shortcuts import render, redirect
-from .forms import UserForm
+
+from accounts.models import CustomUser, Profile
+from store.models import Category, Product
+from voucher.models import Voucher
+from newsletter.models import Subscriber, Newsletter
+from gallery.models import Picture, PictureOfTheMonth
+
+
 
 class UserManagementView(TemplateView):
     template_name = 'mgmt.html'
@@ -41,6 +46,36 @@ class UserCreateView(CreateView):
     success_url = reverse_lazy('management:users')
 
 
+class ProfileListView(ListView):
+    model = Profile
+    context_object_name = 'profile_list'
+    template_name = 'profiles.html'
+
+class ProfileDetailView(DetailView):
+    model = Profile
+    template_name = 'profile_details.html'
+
+
+class ProfileUpdateView(UpdateView):
+    model = Profile
+    fields = ('user', 'role', 'mobile', 'image',)
+    template_name = 'profile_edit.html'
+    success_url = reverse_lazy('management:profiles')
+
+
+class ProfileDeleteView(DeleteView):
+    model = Profile
+    template_name = 'profile_delete.html'
+    success_url = reverse_lazy('management:profiles')
+    
+
+class ProfileCreateView(CreateView):
+    model = Profile
+    fields = ('user', 'role', 'mobile', 'image',)
+    template_name = 'profile_add.html'
+    success_url = reverse_lazy('management:profiles')
+
+
 class ShopManagementView(TemplateView):
     template_name = 'shop_management.html'
 
@@ -74,3 +109,157 @@ class CategoryCreateView(CreateView):
     fields = ('slug', 'name', 'description', 'image', 'image_secondary',)
     template_name = 'category_add.html'
     success_url = reverse_lazy('management:cats')
+
+
+class ProductListView(ListView):
+    model = Product
+    context_object_name = 'product_list'
+    template_name = 'products.html'
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'product_details.html'
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    fields = ('slug', 'name', 'make', 'model', 'description', 'category', 'price', 'stock', 'available', 'image',)
+    template_name = 'product_edit.html'
+    success_url = reverse_lazy('management:products')
+
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    template_name = 'product_delete.html'
+    success_url = reverse_lazy('management:products')
+    
+
+class ProductCreateView(CreateView):
+    model = Product
+    fields = ('slug', 'name', 'make', 'model', 'description', 'category', 'price', 'stock', 'available', 'image',)
+    template_name = 'product_add.html'
+    success_url = reverse_lazy('management:products')
+
+
+
+class VoucherListView(ListView):
+    model = Voucher
+    context_object_name = 'voucher_list'
+    template_name = 'vouchers.html'
+
+class VoucherDetailView(DetailView):
+    model = Voucher
+    template_name = 'voucher_details.html'
+
+
+class VoucherUpdateView(UpdateView):
+    model = Voucher
+    fields = ('code', 'valid_from', 'valid_to', 'discount', 'active',)
+    template_name = 'voucher_edit.html'
+    success_url = reverse_lazy('management:vouchers')
+
+
+class VoucherDeleteView(DeleteView):
+    model = Voucher
+    template_name = 'voucher_delete.html'
+    success_url = reverse_lazy('management:vouchers')
+    
+
+class VoucherCreateView(CreateView):
+    model = Voucher
+    fields = ('code', 'valid_from', 'valid_to', 'discount', 'active',)
+    template_name = 'voucher_add.html'
+    success_url = reverse_lazy('management:vouchers')
+
+
+
+class SubscriberListView(ListView):
+    model = Subscriber
+    context_object_name = 'subscriber_list'
+    template_name = 'subscribers.html'
+
+class SubscriberDetailView(DetailView):
+    model = Subscriber
+    template_name = 'subscriber_details.html'
+
+
+class SubscriberUpdateView(UpdateView):
+    model = Subscriber
+    fields = ('email', 'confirmed',)
+    template_name = 'subscriber_edit.html'
+    success_url = reverse_lazy('management:subscribers')
+
+
+class SubscriberDeleteView(DeleteView):
+    model = Subscriber
+    template_name = 'subscriber_delete.html'
+    success_url = reverse_lazy('management:subscribers')
+    
+
+class SubscriberCreateView(CreateView):
+    model = Subscriber
+    fields = ('email', 'confirmed',)
+    template_name = 'subscriber_add.html'
+    success_url = reverse_lazy('management:subscribers')
+
+
+class NewsletterListView(ListView):
+    model = Newsletter
+    context_object_name = 'newsletter_list'
+    template_name = 'newsletters.html'
+
+class NewsletterDetailView(DetailView):
+    model = Newsletter
+    template_name = 'newsletter_details.html'
+
+
+class NewsletterUpdateView(UpdateView):
+    model = Newsletter
+    fields = ('subject', 'contents',)
+    template_name = 'newsletter_edit.html'
+    success_url = reverse_lazy('management:newsletters')
+
+
+class NewsletterDeleteView(DeleteView):
+    model = Newsletter
+    template_name = 'newsletter_delete.html'
+    success_url = reverse_lazy('management:newsletters')
+    
+
+class NewsletterCreateView(CreateView):
+    model = Newsletter
+    fields = ('subject', 'contents',)
+    template_name = 'newsletter_add.html'
+    success_url = reverse_lazy('management:newsletters')
+
+
+class PictureListView(ListView):
+    model = Picture
+    context_object_name = 'picture_list'
+    template_name = 'newsletters.html'
+
+class PictureDetailView(DetailView):
+    model = Picture
+    template_name = 'newsletter_details.html'
+
+
+class PictureUpdateView(UpdateView):
+    model = Picture
+    fields = ('subject', 'contents',)
+    template_name = 'newsletter_edit.html'
+    success_url = reverse_lazy('management:newsletters')
+
+
+class PictureDeleteView(DeleteView):
+    model = Picture
+    template_name = 'newsletter_delete.html'
+    success_url = reverse_lazy('management:newsletters')
+    
+
+class PictureCreateView(CreateView):
+    model = Picture
+    fields = ('subject', 'contents',)
+    template_name = 'newsletter_add.html'
+    success_url = reverse_lazy('management:newsletters')
+
+
